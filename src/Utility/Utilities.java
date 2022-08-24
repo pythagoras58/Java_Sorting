c604a22f4ab1e9e9bc4fa63b3bc0a3ded4053b48
pythagoras58 <solomondanso58@gmail.com>
2022-08-24 21:19:52 +0000

    Utility Class updated

--- src/Utility/Utilities.java
+++ src/Utility/Utilities.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 
 public class Utilities {
 
+    // iterating a string
     public static void iteratedStringSort(ArrayList<String> musicList) {
         for(String items : musicList){
             System.out.println("[ITEMS]->" + items);
         }
     }
 
+    // iterating integers
     public static void iterateIntegerList(ArrayList<Integer> integerArrayList) {
         for(Integer item : integerArrayList){
             System.out.println("[ITEM] -> " + item);
         }
     }
 
+    // generic iterations
     public static<T> void iterateAnyList(ArrayList<T> integerArrayList) {
         for(T item : integerArrayList){
             System.out.println("[ITEM] -> " + item);
