0f49e5b1d285ad532df204966bfab785ba98edd4
pythagoras58 <solomondanso58@gmail.com>
2022-09-04 17:34:11 +0000

    cal

---
+++ src/MyCalendar.java
@@ -0,0 +1,9 @@
+public class MyCalendar {
+    public static void main(String[] args) {
+        String [] days = {"Mon", "Tues", "Wednes", "Thurs", "Fri", "Sat", "Sun"};
+
+        for(int i=0; i<7; i++){
+            System.out.println(days[i]);
+        }
+    }
+}
