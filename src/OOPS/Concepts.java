702434165f0258bf9e6db9a21877eac9e5cb7256
pythagoras58 <solomondanso58@gmail.com>
2022-09-20 23:02:31 +0000

    List OOP

--- src/OOPS/Concepts.java
+++ src/OOPS/Concepts.java
@@ -1,7 +1,20 @@
 package OOPS;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Concepts {
     public static void main(String[] args) {
 
+        //Lists
+        List<String> names = new ArrayList<>();
+
+        names.add("Abena");
+        names.add("Danso");
+        names.add("Anne");
+
+        names.forEach(person ->{
+            System.out.println("Name is : " + person);
+        });
     }
 }
